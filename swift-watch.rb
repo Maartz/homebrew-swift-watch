1759e7939162b771910f4a59bb2bc3fee87c0637
Maartz <maartz@protonmail.com>
2024-11-17 12:25:23 +0100

    new release

--- swift-watch.rb
+++ swift-watch.rb
@@ -1,9 +1,9 @@
 class SwiftWatch < Formula
   desc 'Swift Watch: Automatically build, test, run, or analyze Swift projects on file changes'
   homepage 'https://github.com/Maartz/homebrew-swift-watch'
-  url 'https://github.com/Maartz/Swift-Watch/archive/refs/tags/1.0.0.tar.gz'
+  url 'https://github.com/Maartz/Swift-Watch/archive/refs/tags/1.0.1.tar.gz'
   version '1.0.0'
-  sha256 '46874d3d72dfa4d07ba1c9532559e12c1969e790c5d46fe91036866f0f034adc'
+  sha256 'a56b30aa77bfbfafbc3ef84639df0fed81505121d1fab743c1a893e0e97ad211'
 
   depends_on 'entr'
 
