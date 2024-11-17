87b506f3606fdb09337cd0e4a292f7f68e31e5a3
Maartz <maartz@protonmail.com>
2024-11-17 11:21:46 +0100

    Add formula

--- swift-watch.rb
+++ swift-watch.rb
@@ -1,7 +1,7 @@
 class SwiftWatch < Formula
   desc 'Swift Watch: Automatically build, test, run, or analyze Swift projects on file changes'
   homepage 'https://github.com/yourusername/swift-watch'
-  url 'https://github.com/Maartz/Swift-Watch/archive/refs/tags/v1.0.0.tar.gz'
+  url 'https://github.com/Maartz/Swift-Watch/archive/refs/tags/1.0.0.tar.gz'
   version '1.0.0'
   sha256 '46874d3d72dfa4d07ba1c9532559e12c1969e790c5d46fe91036866f0f034adc'
 
